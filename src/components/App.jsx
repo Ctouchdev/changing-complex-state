f592a1f95eb1112b7001efef64a917bd912c1b5f
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-21 09:23:22 -0500

    Updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(e) {
+    const newValue = e.target.value;
+    const inputName = e.target.name;
+
+    setFullName((prevValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
-      </form>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+
+      <input
+        onChange={handleChange}
+        name="fName"
+        placeholder="First Name"
+        value={fullName.fName}
+      />
+      <input
+        onChange={handleChange}
+        name="lName"
+        placeholder="Last Name"
+        value={fullName.lName}
+      />
+      <button>Submit</button>
     </div>
   );
 }
